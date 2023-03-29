5ac811216ed0c338f65afe95996dc36573337a7b
sandeep gupta <sandeepguptabc@gmail.com>
2023-03-29 14:11:15 +0530

    upload fix, preview images, routed steps

--- src/components/stepForm/Review.js
+++ src/components/stepForm/Review.js
@@ -1,89 +1,128 @@
 import React from "react";
-import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetail from '@material-ui/core/AccordionDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ListItemText from '@material-ui/core/ListItemText'
-import IconButton from '@material-ui/core/IconButton';
-import EditIcon from '@material-ui/icons/Edit';
-import './form.css'
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetail from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import "./form.css";
 
-export const Review = ({ formData, navigation }) => {
-  const { go } = navigation;
-  const {
-    firstName,
-    lastName,
-    contact,
-    bbrand,
-    btype,
-    srno,
-    vbrand,
-    vtype,
-    vno,
-    claimType,
-    upload1,
-    upload2,
-  } = formData;
+export const Review = ({ formData, navigation, uploadBack, uploadFront }) => {
+	const { go } = navigation;
+	const {
+		firstName,
+		lastName,
+		contact,
+		bbrand,
+		btype,
+		srno,
+		vbrand,
+		vtype,
+		vno,
+		claimType,
+	} = formData;
 
-  return (
-    <Container maxWidth='sm'>
-      <h3>Review</h3>
-      <div className="form-wrapper">
-      <RenderAccordion summary="Names" go={ go } details={[
-        { 'First Name': firstName },
-        { 'Last Name': lastName },
-        { 'Contact No': contact },
-      ]} />
-      <RenderAccordion summary="Battery Details" go={ go } details={[
-        { 'Battery Brand': bbrand },
-        { 'Battery Type': btype },
-        { 'Serial No': srno },
-        { 'Claim Type': claimType },
-      ]} />
-      <RenderAccordion summary="Vehicle Details" go={ go } details={[
-        { 'Vehicle Brand': vbrand },
-        { 'Vehicle Model': vtype},
-        { 'Vehicle Reg-No': vno },
-      ]} />
-      <RenderAccordion summary="Upload" go={ go } details={[
-        { 'Warranty Card Front': upload1 },
-        { 'Warranty Card Back': upload2 },
-      ]} />
-      <Button
-        color="primary"
-        variant="contained"
-        style={{ marginTop: '1.5rem' }}
-        onClick={() => go('submit')}
-      >
-        Submit
-      </Button>
-      </div>
-    </Container>
-  );
+	return (
+		<Container maxWidth="sm">
+			<h3>Review</h3>
+			<div className="form-wrapper">
+				<RenderAccordion
+					summary="Names"
+					go={go}
+					details={[
+						{ "First Name": firstName },
+						{ "Last Name": lastName },
+						{ "Contact No": contact },
+					]}
+				/>
+				<RenderAccordion
+					summary="Battery Details"
+					go={go}
+					details={[
+						{ "Battery Brand": bbrand },
+						{ "Battery Type": btype },
+						{ "Serial No": srno },
+						{ "Claim Type": claimType },
+					]}
+				/>
+				<RenderAccordion
+					summary="Vehicle Details"
+					go={go}
+					details={[
+						{ "Vehicle Brand": vbrand },
+						{ "Vehicle Model": vtype },
+						{ "Vehicle Reg-No": vno },
+					]}
+				/>
+				<RenderAccordion
+					summary="Upload"
+					go={go}
+					details={[
+						{ "Warranty Card Front": uploadFront },
+						{ "Warranty Card Back": uploadBack },
+					]}
+				/>
+				<Button
+					color="primary"
+					variant="contained"
+					style={{ marginTop: "1.5rem" }}
+					onClick={() => go("submit")}>
+					Submit
+				</Button>
+			</div>
+		</Container>
+	);
 };
 
-export const RenderAccordion = ({ summary, details, go }) => (
-  <Accordion style={{ backgroundColor: '#f0f0f0', width: "100%"}}>
-    <AccordionSummary
-      expandIcon={<ExpandMoreIcon />}
-    >{summary}</AccordionSummary>
-    <AccordionDetail>
-      <div>
-        { details.map((data, index) => {
-          const objKey = Object.keys(data)[0];
-          const objValue = data[Object.keys(data)[0]];
+export const RenderAccordion = ({ summary, details, go }) => {
+	const isUpload = summary.startsWith("Upload");
 
-          return <ListItemText key={index}>{`${objKey}: ${objValue}`}</ListItemText>
+	let previewBack;
+	let previewFront;
 
-        }) }
-        <IconButton
-          color="primary"
-          component="span"
-          onClick={() => go(`${summary.toLowerCase()}`)}
-        ><EditIcon /></IconButton>
-      </div>
-    </AccordionDetail>
-  </Accordion>
-)
+	if (isUpload) {
+		const imageFrontFile = Object.values(details[0]);
+		previewFront = URL.createObjectURL(imageFrontFile[0]);
+
+		const imageBackFile = Object.values(details[1]);
+		previewBack = URL.createObjectURL(imageBackFile[0]);
+	}
+
+	return (
+		<Accordion style={{ backgroundColor: "#f0f0f0", width: "100%" }}>
+			<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+				{summary}
+			</AccordionSummary>
+			<AccordionDetail>
+				<div>
+					{isUpload ? (
+						<>
+							<img src={previewFront} width={200} alt="Front" />
+							<img src={previewBack} width={200} alt="back" />
+						</>
+					) : (
+						details.map((data, index) => {
+							const objKey = Object.keys(data)[0];
+							const objValue = data[Object.keys(data)[0]];
+
+							return (
+								<ListItemText
+									key={index}>{`${objKey}: ${objValue}`}</ListItemText>
+							);
+						})
+					)}
+
+					<IconButton
+						color="primary"
+						component="span"
+						onClick={() => go(`${summary.toLowerCase()}`)}>
+						<EditIcon />
+					</IconButton>
+				</div>
+			</AccordionDetail>
+		</Accordion>
+	);
+};
--- src/components/stepForm/Upload.js
+++ src/components/stepForm/Upload.js
@@ -1,118 +1,145 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Dropzone from "react-dropzone";
-import './form.css'
+import "./form.css";
 
-export const Upload = ({ formData, setForm, navigation }) => {
-  const [files1, setFiles1] = useState([]);
-  const [files2, setFiles2] = useState([]);
-  const [errors, setErrors] = useState({});
+export const Upload = ({
+	navigation,
+	setuploadBack,
+	setuploadFront,
+	uploadBack,
+	uploadFront,
+}) => {
+	const [files1, setFiles1] = useState(uploadBack.name ? [uploadBack] : []);
+	const [files2, setFiles2] = useState(uploadFront.name ? [uploadFront] : []);
+	const [errors, setErrors] = useState({});
+	const [previewImageBack, setpreviewImageBack] = useState();
+	const [previewImageFront, setpreviewImageFront] = useState();
 
-  const validate = () => {
-    let errors = {};
-    if (files1.length === 0) {
-      errors.upload1 = "Please select a file";
-    }
-    if (files2.length === 0) {
-      errors.upload2 = "Please select a file";
-    }
-    setErrors(errors);
-    return Object.keys(errors).length === 0;
-  };
+	const validate = () => {
+		let errors = {};
+		if (files1.length === 0) {
+			errors.upload1 = "Please select a file";
+		}
+		if (files2.length === 0) {
+			errors.upload2 = "Please select a file";
+		}
+		setErrors(errors);
+		return Object.keys(errors).length === 0;
+	};
 
-  const handleDrop1 = (acceptedFiles) => {
-    setFiles1(acceptedFiles);
-    setForm({ ...formData, upload1: acceptedFiles });
-  };
+	const handleDrop1 = useCallback(
+		(acceptedFiles) => {
+			setFiles1(acceptedFiles);
+			setuploadBack(acceptedFiles[0]);
+		},
+		[setuploadBack]
+	);
 
-  const handleDrop2 = (acceptedFiles) => {
-    setFiles2(acceptedFiles);
-    setForm({ ...formData, upload2: acceptedFiles });
-  };
+	const handleDrop2 = useCallback(
+		(acceptedFiles, e) => {
+			setFiles2(acceptedFiles);
+			setuploadFront(acceptedFiles[0]);
+		},
+		[setuploadFront]
+	);
 
-  const handleNext = (e) => {
-    e.preventDefault();
-    const isValid = validate();
-    if (isValid) {
-      navigation.next();
-    }
-  };
+	const handleNext = (e) => {
+		e.preventDefault();
+		const isValid = validate();
+		if (isValid) {
+			navigation.next();
+		}
+	};
 
-  return (
-    <Container maxWidth="xs">
-      <h3>Upload Documents</h3>
-      <div className="form-wrapper">
-      <Dropzone onDrop={handleDrop1}>
-        {({ getRootProps, getInputProps }) => (
-          <div style={{ marginTop: "1rem" }}>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag and drop some files here, 1</p>
-            </div>
-            {files1.length > 0 && (
-              <div>
-                <h4>Selected Files:</h4>
-                <ul>
-                  {files1.map((file) => (
-                    <li key={file.name}>
-                      {file.name} - {file.size} bytes
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            {errors.upload1 && (
-              <div style={{ color: "red" }}>{errors.upload1}</div>
-            )}
-          </div>
-        )}
-      </Dropzone>
+	useEffect(() => {
+		if (uploadBack.name) {
+			const objectUrl = URL.createObjectURL(uploadBack);
+			setpreviewImageBack(objectUrl);
+		}
 
-      <Dropzone onDrop={handleDrop2}>
-        {({ getRootProps, getInputProps }) => (
-          <div style={{ marginTop: "1rem" }}>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag and drop some files here, 2</p>
-            </div>
-            {files2.length > 0 && (
-              <div>
-                <h4>Selected Files:</h4>
-                <ul>
-                  {files2.map((file) => (
-                    <li key={file.name}>
-                      {file.name} - {file.size} bytes
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            {errors.upload2 && (
-              <div style={{ color: "red" }}>{errors.upload2}</div>
-            )}
-          </div>
-        )}
-      </Dropzone>
+		if (uploadFront.name) {
+			const objectUrl = URL.createObjectURL(uploadFront);
+			setpreviewImageFront(objectUrl);
+		}
+	}, [uploadBack, uploadFront]);
 
-      <div style={{ marginTop: "1rem" }}>
-        <Button
-          color="neutral"
-          variant="contained"
-          style={{ marginRight: "1rem" }}
-          onClick={() => navigation.previous()}
-        >
-          Back
-        </Button>
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={handleNext}
-        >
-          Next
-        </Button>
-      </div>
-      </div>
-    </Container>
-  );
+	return (
+		<Container maxWidth="xs">
+			<h3>Upload Documents</h3>
+			<div className="form-wrapper">
+				<Dropzone onDrop={handleDrop1}>
+					{({ getRootProps, getInputProps }) => (
+						<div style={{ marginTop: "1rem" }}>
+							<div {...getRootProps()}>
+								<input {...getInputProps()} name="upload1" />
+								<p>Drag and drop some files here, 1</p>
+							</div>
+							{files1.length > 0 && (
+								<div>
+									<h4>Selected Files:</h4>
+									<ul>
+										{files1.map((file) => (
+											<li key={file.name}>
+												{file.name} - {file.size} bytes
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+							{previewImageBack && (
+								<img src={previewImageBack} width={200} alt="back" />
+							)}
+							{errors.upload1 && (
+								<div style={{ color: "red" }}>{errors.upload1}</div>
+							)}
+						</div>
+					)}
+				</Dropzone>
+
+				<Dropzone onDrop={handleDrop2}>
+					{({ getRootProps, getInputProps }) => (
+						<div style={{ marginTop: "1rem" }}>
+							<div {...getRootProps()}>
+								<input {...getInputProps()} name="upload12" />
+								<p>Drag and drop some files here, 2</p>
+							</div>
+							{files2.length > 0 && (
+								<div>
+									<h4>Selected Files:</h4>
+									<ul>
+										{files2.map((file) => (
+											<li key={file.name}>
+												{file.name} - {file.size} bytes
+											</li>
+										))}
+									</ul>
+								</div>
+							)}
+							{previewImageFront && (
+								<img src={previewImageFront} width={200} alt="front" />
+							)}
+							{errors.upload2 && (
+								<div style={{ color: "red" }}>{errors.upload2}</div>
+							)}
+						</div>
+					)}
+				</Dropzone>
+
+				<div style={{ marginTop: "1rem" }}>
+					<Button
+						color="neutral"
+						variant="contained"
+						style={{ marginRight: "1rem" }}
+						onClick={() => navigation.previous()}>
+						Back
+					</Button>
+					<Button color="primary" variant="contained" onClick={handleNext}>
+						Next
+					</Button>
+				</div>
+			</div>
+		</Container>
+	);
 };
